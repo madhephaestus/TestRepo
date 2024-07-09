1e48560daed66e4467efa9cad5337c9b7b641127
Kevin Harrington <kharrington@bancroftschool.org>
2024-07-09 17:29:21 -0400

    Adding test files

--- myFile.groovy
+++ myFile.groovy
@@ -1,1 +1,8 @@
-//Your code here
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cube
+import eu.mihosoft.vrl.v3d.Transform
+
+CSG cube = new Cube(0) (100).toCSG()
+cube.addSlicePlane(new Transform());
+
+return [cube,cube.movez(200)]
