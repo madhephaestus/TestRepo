64e582be82716d5bbb3b3d69cfd0df37f84cb5d5
Kevin Harrington <kharrington@bancroftschool.org>
2024-07-09 18:06:25 -0400

    using the slicing example for Freecad

--- myFile.groovy
+++ myFile.groovy
@@ -1,8 +1,19 @@
+import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
+import com.neuronrobotics.bowlerstudio.vitamins.Vitamins
+
 import eu.mihosoft.vrl.v3d.CSG
 import eu.mihosoft.vrl.v3d.Cube
 import eu.mihosoft.vrl.v3d.Transform
 
-CSG cube = new Cube(0) (100).toCSG()
-cube.addSlicePlane(new Transform());
+File stlToImport =ScriptingEngine.fileFromGit(
+		"https://github.com/NeuronRobotics/NASACurisoity.git"
+		, "STL/upper-arm.STL");
+CSG toSlice = Vitamins.get(stlToImport,true);
+String name="upperArm";
+toSlice.setName(name);
+toSlice.addSlicePlane(new Transform().rotY(90).movex(30));
+toSlice.addSlicePlane(new Transform().movez(toSlice.getMaxZ()-0.5));
+toSlice.addSlicePlane(new Transform().movez(toSlice.getMaxZ()-0.51));
+toSlice.addSlicePlane(new Transform());
 
-return [cube,cube.movez(200)]
+return toSlice
