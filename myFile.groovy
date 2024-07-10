cd53c2ec0d1188db3720f9fb2cadb241b01b55a8
Kevin Bad Name <Kevin Bad Name@10.0.2.15>
2024-07-09 17:30:02 -0700

    Typeo

--- myFile.groovy
+++ myFile.groovy
@@ -2,7 +2,7 @@ import eu.mihosoft.vrl.v3d.CSG
 import eu.mihosoft.vrl.v3d.Cube
 import eu.mihosoft.vrl.v3d.Transform
 
-CSG cube = new Cube(0) (100).toCSG()
+CSG cube = new Cube(100).toCSG()
 cube.addSlicePlane(new Transform());
 
 return [cube,cube.movez(200)]
